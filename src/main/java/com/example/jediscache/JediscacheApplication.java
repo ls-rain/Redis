fdb60cb0ff1fcb141d7a9ace86ae55fd0119f5af
ls-rain <swlee19990115@gmail.com>
2024-05-13 22:35:38 +0900

    spring session 연습

--- src/main/java/com/example/jediscache/JediscacheApplication.java
+++ src/main/java/com/example/jediscache/JediscacheApplication.java
@@ -1,8 +1,14 @@
 package com.example.jediscache;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
+@RestController
 @SpringBootApplication
 public class JediscacheApplication {
 
@@ -10,4 +16,13 @@ public class JediscacheApplication {
 		SpringApplication.run(JediscacheApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public Map<String, String> home(HttpSession session){
+		Integer visitCount = (Integer) session.getAttribute("visits");
+		if(visitCount == null){
+			visitCount = 0;
+		}
+		session.setAttribute("visits", ++visitCount);
+		return Map.of("session id", session.getId(), "visits", visitCount.toString());
+	}
 }
