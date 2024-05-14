8139ff57c350a2c9718e16c20f8ff8da2b9200e1
ls-rain <swlee19990115@gmail.com>
2024-05-14 21:57:31 +0900

    spring data redis pub/sub 연습

---
+++ src/main/java/com/example/jediscache/RedisConfig.java
@@ -0,0 +1,24 @@
+package com.example.jediscache;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    MessageListenerAdapter messageListenerAdapter(){
+        return new MessageListenerAdapter(new MessageListenService());
+    }
+    @Bean
+    RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory connectionFactory,
+                                                                MessageListenerAdapter listener){
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(listener, ChannelTopic.of("users:unregister"));
+        return container;
+    }
+}
